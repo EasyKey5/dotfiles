81e951cf83eb3d2820bd0f3ede4e1d46b583120f
Tama Harrison <tamaharrison061@gmail.com>
2025-01-17 23:09:23 +0000

    add nix and fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    oh-my-posh init fish --config '/Users/tama/.config/fish/themes/tokyonight_storm.omp.json' | source
+end
+
+
+
+alias nv="nvim"
+alias lg="lazygit"
+
+set -g fish_greeting
+function fish_mode_prompt; end
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -10,6 +10,8 @@ config.font_size = 32
 config.unicode_version = 15
 config.enable_tab_bar = false
 config.window_background_opacity = 0.9
+-- config.default_prog = { "fish", "-c", "tmux", "attach", "-t", "base", "||", "tmux", "new", "-s", "base" }
+config.default_prog = { "fish", "-c tmux attach -t base || tmux new -s base" }
 config.window_decorations = "RESIZE"
 
 return config
