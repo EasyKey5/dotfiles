4b33bb6593a416e5134c7d78fe6aed1e862dd390
Tama Harrison <tamaharrison061@gmail.com>
2025-01-19 22:43:50 +0000

    update config

---
+++ fish/.config/fish/functions/rebuild.fish
@@ -0,0 +1,3 @@
+function rebuild --wraps='  darwin-rebuild switch --flake /Users/tama/dotfiles/nix/.config/nix-darwin/' --description '  darwin-rebuild switch --flake /Users/tama/dotfiles/nix/.config/nix-darwin/'
+  darwin-rebuild switch --flake /Users/tama/dotfiles/nix/.config/nix-darwin/
+end
--- nix/.config/nix-darwin/flake.nix
+++ nix/.config/nix-darwin/flake.nix
@@ -5,32 +5,74 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ... }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
+      environment.systemPackages = [ 
+          pkgs.vim
+          pkgs.neovim
+          pkgs.wezterm
+          pkgs.fish
+          pkgs.bat
+          pkgs.fzf
+          pkgs.git
+          pkgs.jq
+          pkgs.lazygit
+          pkgs.lua
+          pkgs.tmux
+          pkgs.yabai
         ];
+
+        homebrew = {
+          enable = true;
+
+          # CLI Tools
+          brews = [
+            "neofetch"
+          ];
+
+          # GUI Apps
+          casks = [
+            "wezterm"
+            # "hammerspoon"
+            # "youtube-music"
+          ];
+      };
+
+
+      # Allow touchID for sudo auth
+      security.pam.enableSudoTouchIdAuth = true;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
       # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
+      programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Create fish config
-      programs.fish.enable = true
-
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
       system.stateVersion = 5;
+
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
@@ -40,7 +82,26 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Tamas-Laptop
     darwinConfigurations."Tamas-Laptop" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "tama";
+              autoMigrate = true;
+
+              # # Optional: Declarative tap management
+              # taps = {
+              #   "homebrew/homebrew-core" = homebrew-core;
+              #   "homebrew/homebrew-cask" = homebrew-cask;
+              #   "homebrew/homebrew-bundle" = homebrew-bundle;
+              # };
+
+            };
+          }
+        ];
     };
 
     # Expose the package set, for convenience
